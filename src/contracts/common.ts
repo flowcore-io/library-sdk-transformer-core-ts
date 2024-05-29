0d82c4fb33be6214a20a55ef7e535e5a90bbbd2a
flowcore-platform <gitops@flowcore.io>
2024-05-29 15:27:00 +0100

    feat: moved general purpose code from nextjs sdk to this sdk

---
+++ src/contracts/common.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const EventDto = z.object({
+  eventId: z.string(),
+  aggregator: z.string(),
+  eventType: z.string(),
+  validTime: z.string(),
+  payload: z.any(),
+});
+
+export const EventMetadataDto = EventDto.omit({
+  payload: true,
+});
+
+export type EventMetdata = z.infer<typeof EventMetadataDto>;
---
+++ src/utils/throw-safe-error.ts
@@ -0,0 +1,18 @@
+/**
+ * Throws a safe error with the provided message and error.
+ * If the error is an instance of Error, the error message is appended to the provided message.
+ * If the error is not an instance of Error, a generic error message is appended to the provided message.
+ *
+ * @param message - The message to be included in the error.
+ * @param error - The error object.
+ * @throws Always throws an error.
+ * @returns This function does not return a value.
+ */
+export const throwSafeError = (message: string, error: unknown): never => {
+  if (error instanceof Error) {
+    throw new Error(`${message}: ${error.message}`);
+  } else {
+    // Error might not be an instance of Error, handle accordingly
+    throw new Error(`${message}: an unexpected error type`);
+  }
+};
