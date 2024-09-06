4d5c20b73b83dacf99732d2e87a7d7bc5985f6ed
René Bischoff <rene.bischoff@gmail.com>
2024-09-06 16:12:35 +0200

    feat! disable unused exports and remove zod

--- src/flowcore/index.ts
+++ src/flowcore/index.ts
@@ -1,4 +1,4 @@
-export * from "./redis-queue"
-export * from "./wait-for-predicate"
+// export * from "./redis-queue"
+// export * from "./wait-for-predicate"
 export * from "./transformer-builder"
 export * from "./webhook-builder"
